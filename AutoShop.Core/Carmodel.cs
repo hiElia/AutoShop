3208897e13acdc86bea1b7aba2fd0c4f065f44d3
Surur Ahmed <hieliasahmed3@gmail.com>
2022-01-22 00:38:54 +0100

    Created class library that represents class & types

---
+++ AutoShop.Core/Carmodel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoShop.Core
+{
+    public class Carmodel
+    {
+       
+            public int id { get; set; }
+            public string brand { get; set; }
+            public string model { get; set; }
+            public int price { get; set; }
+        
+    }
+}
---
+++ AutoShop.Core/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoShop.Core
+{
+   public  class Sale
+    {
+       
+            public int id { get; set; }
+            public int employee_id { get; set; }
+            public int carmodel_id { get; set; }
+        
+    }
+}
